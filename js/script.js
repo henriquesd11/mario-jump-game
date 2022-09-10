6a9b225764653edfbeb6f1a45bda36cc56b5ba14
luiz henrique <henriquesd11@hotmail.com>
2022-09-10 12:59:49 -0300

    finalizando game com os ingredientes principais

--- js/script.js
+++ js/script.js
@@ -3,6 +3,12 @@ const mario = document.querySelector('.mario');
 // pega o conteudo da classe .pipe
 const pipe = document.querySelector('.pipe');
 
+const cloud = document.querySelector('.clouds');
+
+const score = document.querySelector('.score');
+
+const animationScore = document.querySelector('.animation-score');
+
 // função para pular
 const jump = () => {
     // teste commit
@@ -15,13 +21,38 @@ const jump = () => {
 
 // cria um loop infinito para verificar se o mario colidiu com o pipe
 const loop = setInterval(() => {
-    // pega a posição do pipe
+    let valueScore = parseInt(score.innerHTML);
+    score.innerHTML = valueScore + 1;
     const pipePosition = pipe.offsetLeft;
-    if (pipePosition <= 120) {
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const cloudsPosition = +window.getComputedStyle(cloud).left.replace('px', '');
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 100) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './images/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        cloud.style.animation = 'none';
+        cloud.style.left = `${cloudsPosition}px`;
+
+        score.innerHTML = `Game Over <br> Seu score: <u>${valueScore}</u>`
+
+        animationScore.style.display = 'none';
+
+        clearInterval(loop);
     }
 }, 10) // 10 milisegundos
 
 // executa o evento de pulo quando o usuário aperta qualquer tecla
-document.addEventListener('keydown', jump);
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'w' || event.key === 'ArrowUp') {
+        jump();
+    }
+});
+
